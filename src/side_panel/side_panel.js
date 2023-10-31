6c5d39a58163f0fb78cf8f69d870f8975cd1fb58
Desmond Yeoh <desmondyeoh11@gmail.com>
2023-11-01 00:01:40 +0800

    add sidepanel

--- src/side_panel/side_panel.js
+++ src/side_panel/side_panel.js
@@ -3,10 +3,12 @@ console.log("This is a popup!");
 const startBtn = document.getElementById("startBtn");
 const outputDiv = document.getElementById("output");
 startBtn.onclick = async function () {
+  // inject content script
   const currTab = await getCurrentTab();
   chrome.scripting.executeScript({
     target: { tabId: currTab.id },
-    files: ["runScript.js"],
+    func: csHello,
+    args: [csGetArg()],
   });
   outputDiv.innerText = "hi there";
 };
@@ -16,4 +18,11 @@ async function getCurrentTab() {
   // `tab` will either be a `tabs.Tab` instance or `undefined`.
   let [tab] = await chrome.tabs.query(queryOptions);
   return tab;
+}
+
+function csGetArg() {
+  return 1;
+}
+function csHello(a) {
+  console.log("num" + a);
 }
