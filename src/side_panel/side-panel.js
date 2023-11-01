b57271b017c630984432712f8dd57801f9ed57ed
Desmond Yeoh <desmondyeoh11@gmail.com>
2023-11-01 23:02:20 +0800

    format

--- src/side_panel/side-panel.js
+++ src/side_panel/side-panel.js
@@ -7,18 +7,20 @@ startBtn.onclick = async function () {
   const currTab = await getCurrentTab();
   chrome.scripting.insertCSS({
     target: { tabId: currTab.id },
-    files: ['src/side_panel/test.css'],
+    files: ["src/side_panel/test.css"],
   });
-  chrome.scripting.executeScript({
-    target: { tabId: currTab.id },
-    files: ['src/side_panel/jquery-3.7.1.min.js'],
-  }).then(() => {
-    chrome.scripting.executeScript({
+  chrome.scripting
+    .executeScript({
       target: { tabId: currTab.id },
-      func: csHello,
-      args: [csGetArg()],
+      files: ["src/side_panel/jquery-3.7.1.min.js"],
+    })
+    .then(() => {
+      chrome.scripting.executeScript({
+        target: { tabId: currTab.id },
+        func: csHello,
+        args: [csGetArg()],
+      });
     });
-  });
   outputDiv.innerText = "hi there";
 };
 
@@ -33,64 +35,60 @@ function csGetArg() {
   return 1;
 }
 
-
-
-
 function csHello(a) {
-
-
   // Unique ID for the className.
-  var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
+  var MOUSE_VISITED_CLASSNAME = "crx_mouse_visited";
 
   // Previous dom, that we want to track, so we can remove the previous styling.
   var prevDOM = null;
 
-
-  var generateQuerySelector = function(el) {
-    if (el.tagName.toLowerCase() == "html")
-        return "HTML";
+  var generateQuerySelector = function (el) {
+    if (el.tagName.toLowerCase() == "html") return "HTML";
     var str = el.tagName;
-    str += (el.id != "") ? "#" + el.id : "";
+    str += el.id != "" ? "#" + el.id : "";
     if (el.className) {
-        var classes = el.className.split(/\s/);
-        for (var i = 0; i < classes.length; i++) {
-            str += "." + classes[i]
-        }
+      var classes = el.className.split(/\s/);
+      for (var i = 0; i < classes.length; i++) {
+        str += "." + classes[i];
+      }
     }
     return str;
-}
+  };
 
-  var genPair = function(el) {
-    return generateQuerySelector(el.parentNode) + ' > ' + generateQuerySelector(el);
-  }
+  var genPair = function (el) {
+    return (
+      generateQuerySelector(el.parentNode) + " > " + generateQuerySelector(el)
+    );
+  };
 
-  
   // Mouse listener for any move event on the current document.
-document.addEventListener('mousemove', (e) => {
-  // let srcElement = e.srcElement;
-  let srcElement = e.target;
-  console.log(genPair(srcElement));
+  document.addEventListener(
+    "mousemove",
+    (e) => {
+      // let srcElement = e.srcElement;
+      let srcElement = e.target;
+      console.log(genPair(srcElement));
 
-  // Lets check if our underlying element is a IMG.
-  //  && srcElement.nodeName == 'IMG'
-  if (prevDOM != srcElement) {
-
-      // For NPE checking, we check safely. We need to remove the class name
-      // Since we will be styling the new one after.
-      if (prevDOM != null) {
+      // Lets check if our underlying element is a IMG.
+      //  && srcElement.nodeName == 'IMG'
+      if (prevDOM != srcElement) {
+        // For NPE checking, we check safely. We need to remove the class name
+        // Since we will be styling the new one after.
+        if (prevDOM != null) {
           prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
           // console.log(prevDOM.classList)
+        }
+
+        // Add a visited class name to the element. So we can style it.
+        srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
+
+        // The current element is now the previous. So we can remove the class
+        // during the next ieration.
+        prevDOM = srcElement;
+        // console.info(srcElement.currentSrc);
+        // console.dir(srcElement);
       }
-
-
-      // Add a visited class name to the element. So we can style it.
-      srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-
-      // The current element is now the previous. So we can remove the class
-      // during the next ieration.
-      prevDOM = srcElement;
-      // console.info(srcElement.currentSrc);
-      // console.dir(srcElement);
-  }
-}, false);
+    },
+    false
+  );
 }
